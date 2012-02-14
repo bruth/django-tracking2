b024306f79146499f6e86d39c25a025b2f025814
Byron Ruth <b@devel.io>
2012-02-14 10:22:08 -0500

    Catch invalid GET params to prevent server errors while parsing Fix #2

--- tracking/__init__.py
+++ tracking/__init__.py
@@ -3,7 +3,7 @@ __version_info__ = {
     'minor': 1,
     'micro': 0,
     'releaselevel': 'beta',
-    'serial': 5
+    'serial': 6
 }
 
 def get_version(short=False):
--- tracking/views.py
+++ tracking/views.py
@@ -36,16 +36,29 @@ def parse_partial_date(date_str, upper=False):
 @permission_required('tracking.view_visitor')
 def stats(request):
     "Counts, aggregations and more!"
-    start_date = parse_partial_date(request.GET.get('start', None))
-    end_date = parse_partial_date(request.GET.get('end', None), upper=True)
+    errors = []
+    start_date, end_date = None, None
+
+    try:
+        start_str = request.GET.get('start', None)
+        start_date = parse_partial_date(start_str)
+    except (ValueError, TypeError):
+        errors.append('<code>{0}</code> is not a valid start date'.format(start_str))
+
+    try:
+        end_str = request.GET.get('end', None)
+        end_date = parse_partial_date(end_str, upper=True)
+    except (ValueError, TypeError):
+        errors.append('<code>{0}</code> is not a valid end date'.format(end_str))
 
     user_stats = list(Visitor.objects.user_stats(start_date, end_date).order_by('time_on_site_avg'))
-    for u in user_stats:
-        if u.time_on_site_avg is not None:
+    for user in user_stats:
+        if user.time_on_site_avg is not None:
             # Lop off the floating point
-            u.time_on_site_avg = timedelta(seconds=int(u.time_on_site_avg))
+            user.time_on_site_avg = timedelta(seconds=int(user.time_on_site_avg))
 
     return render(request, 'tracking/dashboard.html', {
+        'errors': errors,
         'visitor_stats': Visitor.objects.stats(start_date, end_date),
         'user_stats': user_stats,
         'tracked_dates': Visitor.objects.tracked_dates(),
