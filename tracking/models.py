f2e4aa6318253a9f3d794ccaf0a30a6e115b466f
Yasir Assam <mail@endlessvoid.com>
2013-05-07 13:12:06 +1000

    Removed deprecation warning

--- tracking/models.py
+++ tracking/models.py
@@ -4,7 +4,7 @@ from datetime import datetime, timedelta
 from django.utils import timezone
 from django.contrib.gis.utils import HAS_GEOIP
 if HAS_GEOIP:
-    from django.contrib.gis.utils import GeoIP, GeoIPException
+    from django.contrib.gis.geoip import GeoIP, GeoIPException
 from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import User
