c797e8ba4f0057f50d614ee3c00891a399be5c45
Doug Cox <dougcoxs1@gmail.com>
2014-05-06 12:19:45 -0700

    migrate to textfield, so urls are not cut off

--- tracking/models.py
+++ tracking/models.py
@@ -69,7 +69,7 @@ class Visitor(models.Model):
 
 class Pageview(models.Model):
     visitor = models.ForeignKey(Visitor, related_name='pageviews')
-    url = models.CharField(max_length=500)
+    url = models.TextField(null=False, editable=False)
     method = models.CharField(max_length=20, null=True)
     view_time = models.DateTimeField()
 
