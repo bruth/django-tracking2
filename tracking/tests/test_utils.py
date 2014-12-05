7018271c9f8421f1b6ee5ca2a06b8beab3492a06
Michael J. Schultz <mjschultz@gmail.com>
2014-12-04 18:27:40 -0600

    Add some tests for utils and strip down the code

--- tracking/tests/test_utils.py
+++ tracking/tests/test_utils.py
@@ -1,19 +1,19 @@
 from django.test import TestCase
 
-from tracking.utils import is_valid_ipv4_address, is_valid_ipv6_address
+from tracking.utils import _is_valid_ipv4_address, _is_valid_ipv6_address
 
 
 class UtilsTestCase(TestCase):
 
-    def test_is_valid_ipv4_address(self):
-        self.assertTrue(is_valid_ipv4_address('10.1.1.1'))
-        self.assertTrue(is_valid_ipv4_address('255.255.255.255'))
-        self.assertTrue(is_valid_ipv4_address('0.0.0.0'))
+    def test__is_valid_ipv4_address(self):
+        self.assertTrue(_is_valid_ipv4_address('10.1.1.1'))
+        self.assertTrue(_is_valid_ipv4_address('255.255.255.255'))
+        self.assertTrue(_is_valid_ipv4_address('0.0.0.0'))
 
-        self.assertFalse(is_valid_ipv4_address('256.0.0.257'))
-        self.assertFalse(is_valid_ipv4_address('25600257'))
+        self.assertFalse(_is_valid_ipv4_address('256.0.0.257'))
+        self.assertFalse(_is_valid_ipv4_address('25600257'))
 
-    def test_is_valid_ipv6_address(self):
+    def test__is_valid_ipv6_address(self):
         valid_addrs = (
             'ABCD:EF01:2345:6789:ABCD:EF01:2345:6789',
             '2001:DB8:0:0:8:800:200C:417A',
@@ -27,18 +27,17 @@ class UtilsTestCase(TestCase):
             '0:0:0:0:0:0:13.1.68.3',
             '0:0:0:0:0:FFFF:129.144.52.38',
             '::13.1.68.3',
-            'FFFF:129.144.52.38',
+            '::FFFF:129.144.52.38',
             '2001:0DB8:0000:CD30:0000:0000:0000:0000',
             '2001:0DB8::CD30:0:0:0:0',
             '2001:0DB8:0:CD30::',
         )
         for addr in valid_addrs:
-            self.assertTrue(is_valid_ipv6_address(addr), addr)
+            self.assertTrue(_is_valid_ipv6_address(addr), addr)
 
         invalid_addrs = (
             '2001:0DB8:0:CD3',
-            '2001:0DB8::CD30',
-            '2001:0DB8::CD3',
+            '2001::0DB8::CD30',
         )
         for addr in invalid_addrs:
-            self.assertFalse(is_valid_ipv6_address(addr), addr)
+            self.assertFalse(_is_valid_ipv6_address(addr), addr)
