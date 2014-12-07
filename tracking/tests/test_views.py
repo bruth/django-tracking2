26ce9f01d80bec3abd83c07e97b64d614dcfde6b
Michael J. Schultz <mjschultz@gmail.com>
2014-12-07 15:08:04 -0600

    Test post-logout signal handling

--- tracking/tests/test_views.py
+++ tracking/tests/test_views.py
@@ -1,5 +1,9 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
+from django.utils.timezone import now
+from mock import patch
+
+from tracking.models import Visitor
 
 
 class ViewsTestCase(TestCase):
@@ -21,3 +25,23 @@ class ViewsTestCase(TestCase):
         response = self.client.get(
             '/tracking/?start_time=2014-11&end_time=2014-12-01')
         self.assertEqual(response.status_code, 200)
+
+    @patch('tracking.handlers.timezone.now', autospec=True)
+    def test_logout_tracking(self, mock_end):
+        # logout should call post-logout signal
+        self.now = now()
+        mock_end.return_value = self.now
+
+        # ... but we didn't touch the site
+        self.client.logout()
+        self.assertEqual(Visitor.objects.count(), 0)
+
+        # ... now we have!
+        self.client.login(**self.auth)
+        self.client.get('/tracking/')
+        self.client.logout()
+
+        self.assertEqual(Visitor.objects.count(), 1)
+        visitor = Visitor.objects.get()
+        self.assertEqual(visitor.end_time, self.now)
+        self.assertTrue(visitor.time_on_site > 0)
