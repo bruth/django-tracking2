8f2044c6992534fb59505ad5fe59876bf926f1b9
Michael J. Schultz <mjschultz@gmail.com>
2014-12-08 14:15:39 -0600

    Non-sucky testrunner for pre-django1.6

--- example/example/settings.py
+++ example/example/settings.py
@@ -8,6 +8,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.6/ref/settings/
 """
 
+import django
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
@@ -28,7 +30,8 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-TEST_RUNNER = 'discover_runner.DiscoverRunner'
+if django.get_version() < '1.6':
+    TEST_RUNNER = 'discover_runner.DiscoverRunner'
 
 TRACK_PAGEVIEWS = True
 GEOIP_PATH = os.path.join(BASE_DIR, 'geoip')
