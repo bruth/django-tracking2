7da18db5c7491e4b7a1e95712857d315a9232c82
Michael J. Schultz <mjschultz@gmail.com>
2015-07-01 16:57:42 -0500

    Handle a small race condition in .save() for UPDATE/INSERT fallthrough weirdness

--- tracking/middleware.py
+++ tracking/middleware.py
@@ -2,6 +2,7 @@ import re
 import logging
 import warnings
 
+from django.db import IntegrityError
 from django.utils import timezone
 from django.utils.encoding import smart_text
 
@@ -86,7 +87,16 @@ class VisitorTrackingMiddleware(object):
             time_on_site = total_seconds(visit_time - visitor.start_time)
         visitor.time_on_site = int(time_on_site)
 
-        visitor.save()
+        try:
+            visitor.save()
+        except IntegrityError:
+            # there is a small chance a second response has saved this
+            # Visitor already and a second save() at the same time (having
+            # failed to UPDATE anything) will attempt to INSERT the same
+            # session key (pk) again causing an IntegrityError
+            # If this happens we'll just grab the "winner" and use that!
+            visitor = Visitor.objects.get(pk=session_key)
+
         return visitor
 
     def _add_pageview(self, visitor, request, view_time):
