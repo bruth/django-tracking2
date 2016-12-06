20378e265125dcb2c7fbed0def28fe3805fcad64
Byron Ruth <b@devel.io>
2016-12-06 05:51:55 -0500

    Require Django 1.7 or above
    Signed-off-by: Byron Ruth <b@devel.io>

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ kwargs = {
         exclude=['tests', '*.tests', '*.tests.*', 'tests.*']),
     'include_package_data': True,
     'install_requires': [
-        'django>=1.6',
+        'django>=1.7',
     ],
     'name': 'django-tracking2',
     'version': __import__('tracking').get_version(),
@@ -23,9 +23,7 @@ kwargs = {
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Topic :: Internet :: WWW/HTTP',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
     ],
