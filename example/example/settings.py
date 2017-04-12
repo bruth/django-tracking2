5547249acd1ff6784a0e41be34b98163348a9699
Michael J. Schultz <mjschultz@gmail.com>
2017-04-11 21:22:53 -0500

    Only use new-style MIDDLEWARE for latest django

--- example/example/settings.py
+++ example/example/settings.py
@@ -35,6 +35,17 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.contrib.auth.context_processors.auth',
+                'django.template.context_processors.debug',
+                'django.template.context_processors.i18n',
+                'django.template.context_processors.media',
+                'django.template.context_processors.static',
+                'django.template.context_processors.tz',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
     }
 ]
 
@@ -50,31 +61,32 @@ INSTALLED_APPS = (
     'tracking',
 )
 
-MIDDLEWARE_CLASSES = (
-    # make sure tracking middleware is before SessionMiddleware
-    'tracking.middleware.VisitorTrackingMiddleware',
+if django.VERSION < (1, 10):
+    MIDDLEWARE_CLASSES = (
+        # make sure tracking middleware is before SessionMiddleware
+        'tracking.middleware.VisitorTrackingMiddleware',
 
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-)
+        'django.contrib.sessions.middleware.SessionMiddleware',
+        'django.middleware.common.CommonMiddleware',
+        'django.middleware.csrf.CsrfViewMiddleware',
+        'django.contrib.auth.middleware.AuthenticationMiddleware',
+        'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+        'django.contrib.messages.middleware.MessageMiddleware',
+        'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    )
+else:
+    MIDDLEWARE = [
+        # make sure tracking middleware is before SessionMiddleware
+        'tracking.middleware.VisitorTrackingMiddleware',
 
-MIDDLEWARE = [
-    # make sure tracking middleware is before SessionMiddleware
-    'tracking.middleware.VisitorTrackingMiddleware',
-
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
+        'django.middleware.security.SecurityMiddleware',
+        'django.contrib.sessions.middleware.SessionMiddleware',
+        'django.middleware.common.CommonMiddleware',
+        'django.middleware.csrf.CsrfViewMiddleware',
+        'django.contrib.auth.middleware.AuthenticationMiddleware',
+        'django.contrib.messages.middleware.MessageMiddleware',
+        'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    ]
 
 ROOT_URLCONF = 'example.urls'
 
