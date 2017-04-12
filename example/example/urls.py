8e1b521f569375d57178a618656086225ead0787
Michael J. Schultz <mjschultz@gmail.com>
2017-04-11 21:05:34 -0500

    New-style url includes

--- example/example/urls.py
+++ example/example/urls.py
@@ -1,9 +1,7 @@
 from django.conf.urls import include, url
-
 from django.contrib import admin
-admin.autodiscover()
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', admin.site.urls),
     url(r'^tracking/', include('tracking.urls')),
 ]
