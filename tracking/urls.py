7d9229e083b1c126baa4e44221ad62beefcb343d
Ed Turpin <eturpin@live.com>
2019-07-01 16:55:57 -0500

    In progress of adding views.

--- tracking/urls.py
+++ tracking/urls.py
@@ -3,9 +3,11 @@ from django.conf.urls import url
 from tracking.views import (
     dashboard,
     visitor_overview,
+    visitor_visits,
 )
 
 urlpatterns = [
     url(r'^$', dashboard, name='tracking-dashboard'),
     url(r'^visitors/(?P<user_id>.*)/$', visitor_overview, name='tracking-visitor-overview'),
+    url(r'^visits/(?P<visit_id>.*)/$', visitor_visits, name='tracking-visitor-visits'),
 ]
